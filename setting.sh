12cff81e631d2aa35e73dd5a17379ee25bf769d9
thomasfernandas <98635469+thomasfernandas@users.noreply.github.com>
2022-01-29 22:16:21 +0530

    Update setting.sh

--- setting.sh
+++ setting.sh
@@ -1,6 +1,6 @@
 sudo apt-get update
-sudo apt-get install xfce4 xfce4-terminal
-sudo apt install tigervnc-standalone-server tigervnc-xorg-extension tigervnc-viewer -y
+sudo apt-get install xfce4 xfce4-terminal -y > /dev/null 2>&1
+sudo apt install tigtvncserver expect -y
+expect -c 'set timeout 3;spawn /usr/bin/vncpasswd;expect "*?assword:*";send -- "123456\r";expect "*?erify:*";send -- "123456\r";expect "*?view-only password*";send -- "n\r";send -- "\r";expect eof' && \
 
-echo -e "123456" >>  ~/.vnc/
 vncserver -depth 32 -geometry 1680x1050
